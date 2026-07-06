4ee0610d4d422765c97cde41088d8fc332295a25
ijazhossain <ijaz.hossain35@gmail.com>
2026-07-06 11:14:06 +0600

    token ytils added

--- src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
+++ src/app/(commonLayout)/(authRouteGroup)/login/_action.ts
@@ -1,27 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { httpClient } from "@/lib/axios/httpClient";
+import { setTokenInCookies } from "@/lib/tokenUtils";
 import { ApiErrorResponse } from "@/types/api.types";
 import { ILoginResponse } from "@/types/auth.types";
 import { ILoginPayload, loginZodSchema } from "@/zod/auth.validation";
+import { redirect } from "next/navigation";
 
-export const loginAction=async(payload:ILoginPayload):Promise<ILoginResponse | ApiErrorResponse>=>{
-    const parsedPayload=loginZodSchema.safeParse(payload);
-    if(!parsedPayload.success){
-        const firstError=parsedPayload.error.issues[0].message || "Invalid Input";
-        return{
-            success:false,
-            message:firstError,
-            
-        }
-    }
-    try{
- const response=await httpClient.post<ILoginResponse>("/auth/login",parsedPayload.data);
- const {accessToken,refreshToken,token,user}=response.data;
- return response.data;
-    }catch(error:any){
-return{
-    message:`Login failed:${error.message}`,
-    success:false,
-}
-    }
-}
+export const loginAction = async (
+  payload: ILoginPayload,
+): Promise<ILoginResponse | ApiErrorResponse> => {
+  const parsedPayload = loginZodSchema.safeParse(payload);
+  if (!parsedPayload.success) {
+    const firstError = parsedPayload.error.issues[0].message || "Invalid Input";
+    return {
+      success: false,
+      message: firstError,
+    };
+  }
+  try {
+    const response = await httpClient.post<ILoginResponse>(
+      "/auth/login",
+      parsedPayload.data,
+    );
+    const { accessToken, refreshToken, token } = response.data;
+    await setTokenInCookies("accessToken", accessToken);
+    await setTokenInCookies("refreshToken", refreshToken);
+    await setTokenInCookies("better-auth.session_token", token);
+    redirect("/dashboard");
+    return response.data;
+  } catch (error: any) {
+    return {
+      message: `Login failed:${error.message}`,
+      success: false,
+    };
+  }
+};
