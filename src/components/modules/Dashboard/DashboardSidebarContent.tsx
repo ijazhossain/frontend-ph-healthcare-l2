01f6f2ea40c089f9b35adea836162e697c70b985
ijazhossain <ijaz.hossain35@gmail.com>
2026-07-31 15:47:30 +0600

    making dashboard sidebar component

--- src/components/modules/Dashboard/DashboardSidebarContent.tsx
+++ src/components/modules/Dashboard/DashboardSidebarContent.tsx
@@ -4,6 +4,9 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { NavSection } from "@/types/dashboard.types";
 import { UserInfo } from "@/types/user.types";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Home } from 'lucide-react'
+import { cn } from "@/lib/utils";
 
 interface DashboardSidebarContentProps {
   userInfo: UserInfo;
@@ -15,6 +18,7 @@ const DashboardSidebarContent = ({
   navItems,
   userInfo,
 }: DashboardSidebarContentProps) => {
+    const pathname=usePathname()
   return (
     <div className="hidden h-16 items-center border-b px-6">
       {/* Logo/Brand */}
@@ -24,8 +28,44 @@ const DashboardSidebarContent = ({
         </Link>
       </div>
       {/* Navigation area */}
-<ScrollArea>
+<ScrollArea className="flex-1 px-3 py-4">
     
+            <nav className="space-y-6">
+              {navItems.map((section,sectionId)=>(<div key={sectionId}>
+                            {
+                                section.title && (
+                                    <h4 className="mb-2 px-3 text-xs font-semibold text-muted-foreground uppercase tracking-wider">{section.title}</h4>
+                                )
+                            }
+                            <div className="space-y-1">
+                                {
+                                  section.items.map((item,id)=>{
+                                    const isActive=pathname===item.href;
+                                    //Icon mapper function
+                                    // const Icon=<Home/>
+                                    return(
+                                      <Link href={item.href} key={id} className={
+                                        cn("flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-all",
+                                          isActive ? "bg-primary text-primary-foreground":"text-muted-foreground hover:bg-accent hover:text-accent-foreground"
+                                        )
+                                      }>
+                                      <Home/>
+                                      <span>{item.title}</span>
+                                      </Link>
+                                    )
+
+                                  })  
+                                }
+                            </div>
+                        </div>
+                            
+                        ))
+                    }
+            </nav>
+            
+                
+       
+        
 </ScrollArea>
       {/* User Info at bottom */}
       <div className="border-t px-3 py-4">
