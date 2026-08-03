a738b11869178453b0d434ef16afb360de6560b6
ijazhossain <ijaz.hossain35@gmail.com>
2026-08-03 17:23:33 +0600

    reusable table added

--- src/components/modules/Admin/DoctorsManagement/DoctorsTable.tsx
+++ src/components/modules/Admin/DoctorsManagement/DoctorsTable.tsx
@@ -1,33 +1,45 @@
 "use client";
 
+import DataTable from "@/components/shared/table/DataTable";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { getDoctors } from "@/services/doctor.services";
+import { IDoctor } from "@/types/doctor.types";
 import { useQuery } from "@tanstack/react-query";
 import {
+    ColumnDef,
     flexRender,
     getCoreRowModel,
     useReactTable
 } from "@tanstack/react-table";
+import { doctorColumns } from "./doctorColumns";
 
 
 const DoctorsTable = () => {
 
-    const doctorColumns = [
-      { accessorKey: "name", header: "Name" },
-    //   { accessorKey: "specialization", header: "Specialization" },
-      { accessorKey: "experience", header: "Experience" },
-    //   { accessorKey: "rating", header: "Rating" },
-    ];
+    // const doctorColumns:ColumnDef<IDoctor>[] = [
+    //   { accessorKey: "name", header: "Name" },
+    // //   { accessorKey: "specialization", header: "Specialization" },
+    //   { accessorKey: "experience", header: "Experience" },
+    // //   { accessorKey: "rating", header: "Rating" },
+    // ];
 
    
 
-    const { data : doctorDataResponse } = useQuery({
+    const { data : doctorDataResponse,isLoading } = useQuery({
         queryKey: ["doctors"],
         queryFn: getDoctors
     });
 
     const { data : doctors } = doctorDataResponse! || [];
-
+const handleView=(doctor:IDoctor)=>{
+    console.log("View doctor", doctor);
+}
+const handleEdit=(doctor:IDoctor)=>{
+    console.log("Edit doctor", doctor);
+}
+const handleDelete=(doctor:IDoctor)=>{
+    console.log("Delete doctor", doctor);
+}
 
     const { getHeaderGroups, getRowModel } = useReactTable({
        data: doctors,
@@ -35,37 +47,24 @@ const DoctorsTable = () => {
        getCoreRowModel: getCoreRowModel(),
     });   
 
-    // console.log(doctorDataResponse?.data.map(doctor => doctor.name));
+    console.log(doctorDataResponse?.data.map(doctor => doctor.name));
 
     console.log(doctors);
   return (
-    <Table>
-      <TableHeader>
-        {getHeaderGroups().map((hg) => (
-          <TableRow key={hg.id}>
-            {hg.headers.map((header) => (
-              <TableHead key={header.id}>
-                {flexRender(
-                  header.column.columnDef.header,
-                  header.getContext(),
-                )}
-              </TableHead>
-            ))}
-          </TableRow>
-        ))}
-      </TableHeader>
-      <TableBody>
-        {getRowModel().rows.map((row) => (
-          <TableRow key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <TableCell key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+   <DataTable 
+   data={doctors}
+   columns={doctorColumns}
+   isLoading={isLoading}
+   emptyMessage="No doctors found"
+   actions={
+{
+    onView:handleView,
+    onEdit:handleEdit,
+    onDelete:handleDelete
+
+}
+   }
+   />
   );
 }
 
---
+++ src/components/shared/cell/StatusBadgeCell.tsx
@@ -0,0 +1,14 @@
+import { Badge } from "@/components/ui/badge";
+import { UserStatus } from "@/types/doctor.types";
+
+interface IStatusBadgeCellProps{
+    status:UserStatus;
+}
+const StatusBadgeCell=({status}:IStatusBadgeCellProps)=>{
+    return(
+        <Badge variant={status===UserStatus.ACTIVE?"default": status===UserStatus.BLOCKED?"destructive":"secondary"}>
+            <span className="text-sm capitalize">{status.toLowerCase()}</span>
+        </Badge>
+    )
+}
+export default StatusBadgeCell;
